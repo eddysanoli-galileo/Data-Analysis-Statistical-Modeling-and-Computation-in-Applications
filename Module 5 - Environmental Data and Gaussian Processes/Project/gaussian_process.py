1454e65e68743629d9710dad0074c3c0609e952c
Eduardo Santizo <eddysanoli@gmail.com>
2023-05-08 05:48:52 -0600

    Module 5 - Project: Problem 5?
    Finished the prediction of new data. However, something seems to be going horribly wrong, as my predictions are going way overboard and dont fit to the actual data in any way. However, I finished the prediction routine in order to facilitate its use. Also removed the manual calculation of the kernel inside of the optimization routine, and moved said routine to using the "predict_conditional_mean_and_variance" function. Also included a new fix that now transforms the values of X provided to the function into indices that it can use to index sigma freely, instead of using X directly and just assuming that X should consist of indices.

--- Module 5 - Environmental Data and Gaussian Processes/Project/gaussian_process.py
+++ Module 5 - Environmental Data and Gaussian Processes/Project/gaussian_process.py
@@ -26,6 +26,146 @@ def squared_exp_kernel(X, l, sigma):
     return K
 
 # ============================================== #
+# PREDICT CONDITIONAL MEAN AND VARIANCE          #
+# ============================================== #
+
+
+def predict_conditional_mean_and_var(
+    x1: np.ndarray,
+    x2: np.ndarray,
+    y2: np.ndarray,
+    kernel_args: tuple,
+    kernel: Callable[..., np.ndarray] = squared_exp_kernel,
+    tau: float = 0.001,
+    moving_average_window_size: int = 5,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Predict the conditional mean and variance of the test data, given the train data.
+
+    Parameters
+    ----------
+    x1 : np.ndarray
+        Array containing the indices or "X" values of the data that we want to make 
+        predictions for. During training this will consist of the X values of the
+        test data.
+
+    x2 : np.ndarray
+        Array containing the indices or "X" values of the data that we want to use
+        as reference for the predictions. During training this will consist of the
+        X values of the train data.
+
+    y2 : np.ndarray
+        Array containing the actual measurements or "Y" values of the data that we
+        want to use as reference for the predictions. During training this will 
+        consist of the Y values of the train data (e.g. temperature, flow, speed, etc.).
+
+    tau : float, optional
+        Parameter indicating the variance of the noise in observations. For the
+        Philippines dataset, this is set to 0.001 by default.
+
+    moving_average_window_size : int, optional
+        Size of the window to use for the moving average. Number of subsequent values
+        to use for the moving average.
+    """
+
+    # Get the moving average of the training data to use as the
+    # mean for the Gaussian Process
+    mu_1 = np.convolve(
+        x1,
+        np.ones(moving_average_window_size)/moving_average_window_size,
+        mode='same'
+    )
+    mu_2 = np.convolve(
+        x2,
+        np.ones(moving_average_window_size)/moving_average_window_size,
+        mode='same'
+    )
+
+    # ============== X1 AND X2 INDICES ============= #
+
+    # Get the ordered indexes for x1 and x2
+    #
+    # Lets say we have the following arrays:
+    # - x1 = [1, 2, 3, 4, 5]
+    # - x2 = [0.5, 1, 1.5, 2, 2.5]
+    #
+    # We merge them by concatenating them:
+    #   idx  0  1  2  3  4   5   6   7   8   9
+    # - x = [1, 2, 3, 4, 5, 0.5, 1, 1.5, 2, 2.5]
+    #
+    # We create a mask to separate the x1 and x2 values:
+    # - mask = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+    #
+    # We sort the X array in ascending order and get the resulting indexes:
+    # - sorting_indexes = [  5, 0, 6,   7, 1, 8,   9, 2, 3, 4]
+    #          x values    0.5  1  1  1.5  2  2  2.5  3  4  5
+    #
+    # We sort the mask to check where the x1 and x2 ended up:
+    # - sorted_mask = [1, 0, 1, 1, 0, 1, 1, 0, 0, 0]
+    #
+    # We assign new indexes to the sorted x array
+    # - ind_x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    #
+    # Finally, we get the new indexes of x that correspond to x1 and x2
+    # (x1 has mask values of 0, x2 has mask values of 1)
+    # - ind_x1 = [0, 2, 3, 5, 6]
+    # - ind_x2 = [1, 4, 7, 8, 9]
+
+    # Build an X array that groups all possible values of x1 and x2
+    x = np.concatenate((x1, x2))
+
+    # Create a mask to separate the x1 and x2 values
+    mask_x1 = np.zeros(len(x1))
+    mask_x2 = np.ones(len(x2))
+    mask = np.concatenate((mask_x1, mask_x2))
+
+    # We get the indexes that would sort the X array in ascending order
+    sorting_indexes = np.argsort(x)
+
+    # We sort both the X array and its mask
+    sorted_mask = mask[sorting_indexes]
+
+    # Assign new indexes to the sorted X array
+    ind_x = np.arange(len(x))
+
+    # Fetch the new indexes that correspond to x1 and x2
+    ind_x1 = ind_x[sorted_mask == 0]
+    ind_x2 = ind_x[sorted_mask == 1]
+
+    # =========== CONDITIONAL ESTIMATION =========== #
+
+    # Calculate the Sigma matrix from the kernel. This will be used as the
+    # covariance matrix for the Gaussian Process
+    sigma = kernel(x, *kernel_args)
+
+    # Get each of the parts of the Sigma matrix:
+    # - Sigma_11: Variance of the test data
+    # - Sigma_12: Covariance between the test and train data
+    # - Sigma_21: Covariance between the train data and the test data
+    # - Sigma_22: Variance of the train data
+    # (The weird indexing is to get the correct shape for the matrix, Sigma
+    # is still a 2D array but the matrices inside are not in a fixed location
+    # due to the k-fold splitting)
+    sigma_11 = sigma[ind_x1][:, ind_x1]
+    sigma_12 = sigma[ind_x1][:, ind_x2]
+    sigma_21 = sigma[ind_x2][:, ind_x1]
+    sigma_22 = sigma[ind_x2][:, ind_x2]
+
+    # Compute the estimated noise in the variance of the training data
+    # The resulting matrix should have the same shape as Sigma_22
+    sigma_22_noise = sigma_22 + tau * np.eye(len(sigma_22))
+
+    # Compute the conditional mean and variance of the test data,
+    # given the train data
+    mu_1_given_2 = mu_1 + \
+        sigma_12 @ np.linalg.inv(sigma_22_noise) @ (y2 - mu_2)
+    sigma_1_given_2 = sigma_11 - \
+        sigma_12 @ np.linalg.inv(sigma_22_noise) @ sigma_21
+
+    return mu_1_given_2, sigma_1_given_2, sigma_22_noise
+
+
+# ============================================== #
 # OPTIMIZE KERNEL PARAMS                         #
 # ============================================== #
 
@@ -36,7 +176,7 @@ def optimize_kernel_params(
     std_range: np.ndarray,
     tau: float = 0.001,
     num_folds: int = 10,
-    kernel: Optional[Callable[..., np.ndarray]] = None,
+    kernel: Callable[..., np.ndarray] = squared_exp_kernel,
 ) -> Tuple[float, float, pd.DataFrame]:
     """
     Optimize the kernel parameters for the Gaussian Process.
@@ -80,11 +220,6 @@ def optimize_kernel_params(
     # Each row will consist of a different combination of parameters
     param_pairs = np.stack([l_list, std_list], axis=1)
 
-    # =================== KERNEL =================== #
-
-    if kernel is None:
-        kernel = squared_exp_kernel
-
     # =========== PARAMETER OPTIMIZATION =========== #
 
     # List of optimization results
@@ -99,15 +234,6 @@ def optimize_kernel_params(
         l = params[0]
         std = params[1]
 
-        # Get the full range of values for X
-        # (For the Philippines dataset, this is just the X axis of the data, or
-        # a range from 0 to the number of timesteps)
-        X = np.arange(len(data))
-
-        # Calculate the Sigma matrix from the kernel. This will be used as the
-        # covariance matrix for the Gaussian Process
-        Sigma = squared_exp_kernel(X, l, std)
-
         # ============== CROSS VALIDATION ============== #
 
         # Create the k-fold object
@@ -121,43 +247,15 @@ def optimize_kernel_params(
             # Get the training and test data for the current fold
             y_train, y_test = data[x_train], data[x_test]
 
-            # Get the moving average of the training data to use as the
-            # mean for the Gaussian Process
-            window_size = 5
-            mu_1 = np.convolve(
-                y_test,
-                np.ones(window_size)/window_size,
-                mode='same'
+            # Predict the conditional mean and variance of the test data
+            mu_1_given_2, sigma_1_given_2, sigma_22_noise = predict_conditional_mean_and_var(
+                x1=x_test,
+                x2=x_train,
+                y2=y_train,
+                tau=tau,
+                kernel_args=(l, std),
+                kernel=kernel,
             )
-            mu_2 = np.convolve(
-                y_train,
-                np.ones(window_size)/window_size,
-                mode='same'
-            )
-
-            # Get each of the parts of the Sigma matrix:
-            # - Sigma_11: Variance of the test data
-            # - Sigma_12: Covariance between the test and train data
-            # - Sigma_21: Covariance between the train data and the test data
-            # - Sigma_22: Variance of the train data
-            # (The weird indexing is to get the correct shape for the matrix, Sigma
-            # is still a 2D array but the matrices inside are not in a fixed location
-            # due to the k-fold splitting)
-            Sigma_11 = Sigma[x_test][:, x_test]
-            Sigma_12 = Sigma[x_test][:, x_train]
-            Sigma_21 = Sigma[x_train][:, x_test]
-            Sigma_22 = Sigma[x_train][:, x_train]
-
-            # Compute the estimated noise in the variance of the training data
-            # The resulting matrix should have the same shape as Sigma_22
-            Sigma_22_noise = Sigma_22 + tau * np.eye(len(Sigma_22))
-
-            # Compute the conditional mean and variance of the test data,
-            # given the train data
-            mu_1_given_2 = mu_1 + \
-                Sigma_12 @ np.linalg.inv(Sigma_22_noise) @ (y_train - mu_2)
-            Sigma_1_given_2 = Sigma_11 - \
-                Sigma_12 @ np.linalg.inv(Sigma_22_noise) @ Sigma_21
 
             # Parameters for the log-likelihood function
             N_minus_d = len(y_train)
@@ -166,9 +264,9 @@ def optimize_kernel_params(
             # Compute the log-likelihood of the test data given the train data
             # for the current k-fold
             term_1 = -np.log((2 * np.pi)**((N_minus_d / k)/2) *
-                             np.linalg.det(Sigma_22_noise)**(1/2))
+                             np.linalg.det(sigma_22_noise)**(1/2))
             term_2 = 0.5 * (y_test - mu_1_given_2).T @ \
-                np.linalg.inv(Sigma_1_given_2) @ (y_test - mu_1_given_2)
+                np.linalg.inv(sigma_1_given_2) @ (y_test - mu_1_given_2)
 
             k_fold_log_likelihood = term_1 - term_2
 
